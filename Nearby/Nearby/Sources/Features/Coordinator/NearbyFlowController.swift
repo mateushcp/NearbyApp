35258441389537522582d8091b6e1e1d767b7dc1
Mateus Coelho <56733190+mateushcp@users.noreply.github.com>
2024-11-06 09:15:34 -0300

    Add files via upload

---
+++ Nearby/Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -0,0 +1,36 @@
+//
+//  NearbyFlowController.swift
+//  Nearby
+//
+//  Created by Mateus Henrique Coelho de Paulo on 04/11/24.
+//
+
+import Foundation
+import UIKit
+
+
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init() {
+    }
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViewController = SplashViewController(contentView: contentView, delegate: self)
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        
+        return navigationController
+    }
+    
+}
+
+extension NearbyFlowController: SplashFlowDelegate {
+    func decideNavigationFlow() {
+        let contentView = WelcomeView()
+        let welcomeViewController = WelcomeViewController(contentView: contentView)
+        navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
+    
+}
+
